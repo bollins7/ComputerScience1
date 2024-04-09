7b52adba078094ad11e2243ad50e52b098524369
bollins <ccollins0410@gmail.com>
2024-04-09 18:11:28 -0500

    finished program, waiting to compile and test

--- class_of_students/classStudents.cpp
+++ class_of_students/classStudents.cpp
@@ -6,4 +6,24 @@ using namespace std;
 
 int main (){
     string name, front, back;
+
+    ifstream file("LineUp.txt");
+
+    if (file.is_open()) {
+        file >> name;
+        front = back = name;
+
+        while (file >> name) {
+            if (name < front) {
+                front = name;
+            }
+            if (name > back) {
+                back = name;
+            }
+        }
+        file.close();
+
+        cout << front << " is in the front" << endl;
+        cout << back << " is in the back" << endl;
+    }
 }
